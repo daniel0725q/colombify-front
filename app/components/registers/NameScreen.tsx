0eb2f86d907aeaa8d5136e2198ae437c846ccfc3
Vladimir Ortega <animatrix88@gmail.com>
2024-10-02 21:08:16 -0500

    Cambios en la pantalla del nombre como campo si esta vacio

--- app/components/registers/NameScreen.tsx
+++ app/components/registers/NameScreen.tsx
@@ -15,6 +15,18 @@ type NavigationProp = StackNavigationProp<RootStackParamList, 'EmailScreen'>;
 
 export default function NameScreen() {
   const [name, setName] = useState<string>('');
+  const [nameError, setNameError] = useState<string | null>(null);
+
+  const handleNext = async () => {
+    // Validar si el nombre no está vacío
+    if (name.trim() === '') {
+      setNameError("El nombre no es válido");
+    } else {
+      setNameError(null); // No hay errores, limpiar mensaje de error
+      await AsyncStorage.setItem("Name", name); // Guardar nombre en AsyncStorage
+      router.navigate("./EmailScreen"); // Navegar a la siguiente pantalla
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -34,17 +46,13 @@ export default function NameScreen() {
         }}
       /> */}
       <Pressable
-        onPress={async () => {
-          router.navigate("./EmailScreen");
-          await AsyncStorage.setItem("Name", name);
-        }}
-        
+        onPress={handleNext}
         style={[
           styles.roundedButton,
           styles.blueButton,
-          { opacity: name  ? 1 : 0.5 }  // Cambia la opacidad si no están completos
+          { opacity: name ? 1 : 0.5 }, // Cambia opacidad si el nombre está vacío
         ]}
-        disabled={!name}  // Deshabilita el botón si alguno está vacío
+        disabled={!name} // Deshabilitar el botón si no hay nombre
       >
         <Text style={styles.buttonText}>Siguiente</Text>
       </Pressable>
@@ -90,4 +98,21 @@ const styles = StyleSheet.create({
     marginTop: -300,  // Agregar margen superior para bajarlo
 
   },
+  errorText: {
+    color: 'red',
+    marginTop: 8,
+  },
+  inputError: {
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#9C9C9C',
+  }
 });
